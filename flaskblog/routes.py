ea40edb7fd18ba4efa32eb6f02ce3d9e108c0976
Mahfuz Ahmed <mah3uz@gmail.com>
2018-10-09 17:49:42 +0600

    Create Post

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -3,7 +3,7 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, logout_user, current_user, login_required
 
@@ -14,26 +14,13 @@ PROFILE_PIC_SIZE = (125, 125)
 POST_THUMB_SIZE = (225, 150)
 POST_IMG_SIZE = (500, 300)
 
-posts = [
-    {
-        'title': 'The first post.',
-        'author': 'Mahfuz Shaikh',
-        'content': 'This is the content of post.',
-        'post_date': '04 Sep, 2018'
-    },
-    {
-        'title': 'Another post.',
-        'author': 'John Doe',
-        'content': 'This is the content of another post.',
-        'post_date': '12 Aug, 2018'
-    }
-]
-
 
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
+
 
 def image_upload(image, path, size):
     random_hex = secrets.token_hex(8)
@@ -127,6 +114,33 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your Post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form)
+
+
+@app.route("/post/<int:post_id>", methods=['GET', 'POST'])
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+
+
+
+
+
+
 
 
 
