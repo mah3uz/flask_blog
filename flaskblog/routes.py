a7586be171fdef4ca81cc9eda5e479a4937c3188
Mahfuz Ahmed <mah3uz@gmail.com>
2018-10-13 13:50:13 +0600

    Update & Delete Post

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
@@ -121,12 +121,14 @@ def logout():
 def new_post():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        post = Post(title=form.title.data,
+                    content=form.content.data,
+                    author=current_user)
         db.session.add(post)
         db.session.commit()
         flash('Your Post has been created!', 'success')
         return redirect(url_for('home'))
-    return render_template('create_post.html', title='New Post', form=form)
+    return render_template('create_post.html', title='New Post', form=form, legend='New Post')
 
 
 @app.route("/post/<int:post_id>", methods=['GET', 'POST'])
@@ -135,8 +137,43 @@ def post(post_id):
     return render_template('post.html', title=post.title, post=post)
 
 
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+
+    form = PostForm()
+
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+
+        flash('Your Post has been updated successfuly!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+        form.submit.label.text = 'Update'
+
+    return render_template('create_post.html',
+                           title='Update Post',
+                           form=form,
+                           legend='Update Post')
 
 
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your Post has been deleted successfuly!', 'warning')
+    return redirect(url_for('home'))
 
 
 
